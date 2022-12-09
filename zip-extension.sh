84f0722819c95dafe463b2f69c2bed378616d80f
Maria Malysheva <mn.malysheva@mail.ru>
2022-12-09 05:13:22 +0400

    Add promo tiles

--- zip-extension.sh
+++ zip-extension.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r learn-armenian-alphabet-chrome-extension.zip . -x ".idea/*" -x "*.git*" -x "LICENSE" -x "*.sh"
+zip -r learn-armenian-alphabet-chrome-extension.zip . -x ".idea/*" -x "*.git*" -x "LICENSE" -x "images/*" -x "*.sh"
